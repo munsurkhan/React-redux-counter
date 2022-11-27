77d70e9dd1403fabec101364b56c3ab71bb778bd
Munsur Rahman <munsurinfo@gmail.com>
2022-11-27 19:58:21 +0600

    8

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "../src/Assets/css/app.css";
 import App from './App';
+import {Provider} from "react-redux";
+import store from "./Assets/Redux/store/Store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+            <App />
+      </Provider>
   </React.StrictMode>
 );
 
